454ae764fd7c8a4cfdc0629872b5e525c5c3af8e
FiqihPavitaA <fiqih.pavita@gmail.com>
2023-10-12 10:08:54 +0700

    updated

--- config/db.config.js
+++ config/db.config.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2')
 const fs = require('fs')
 const path = require('path')
 
-const conn = new Sequelize("mysql://avnadmin:AVNS_oxuen7J32drJFXNfyb4@mysql-952c8d8-db-jayapura-31.aivencloud.com:26218/defaultdb?ssl-mode=REQUIRED", {
-    ssl: fs.readFileSync(path.join(__dirname, 'ca (1).pem')),
+const conn = new Sequelize("mysql://avnadmin:AVNS_3I2X7_xYd-OEOl07Fl8@talkspace-fiqih-5386.aivencloud.com:27136/defaultdb?ssl-mode=REQUIRED", {
+    ssl: fs.readFileSync(path.join(__dirname, 'ca.pem')),
     dialect: 'mysql',
     
   });
